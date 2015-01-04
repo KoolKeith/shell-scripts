51f79c6446d3a831424bb01b1260ee778c2d6a61
Natenom <natenom@natenom.com>
2015-01-04 09:48:44 +0100

    very simple "GUI" for mumble starter with kdialog :)

---
+++ mumble/km.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# "GUI" tool to start Mumble :)
+_kdialogtext="Choose your mumble client..."
+_kdialogstring=""
+APPDIR="/home/apps/mumble"
+mumbles=""
+
+function readAvailableMumbles() {
+    local _count=0
+    for _entry in $(ls -D "${APPDIR}") #List only directories. #Does not work with spaces in dirnames.
+    do  
+        mumbles[$_count]="${_entry}"
+        _kdialogstring="${_kdialogstring} ${_count} \"${_entry}\" off"
+	_count=$(($_count+1))
+    done
+}
+
+readAvailableMumbles
+
+_selection=$(kdialog --radiolist "$_kdialogtext" $_kdialogstring)
+
+if [ "$_selection" == "" ];
+then
+    echo "No selection made. Quitting..."
+    exit 1
+else
+    cd "${APPDIR}/${mumbles[${_selection}]}"
+    LD_LIBRARY_PATH=./ ./mumble -m $@
+    exit 0
+fi	
