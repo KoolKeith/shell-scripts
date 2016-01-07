4fbbbc07fe8faba12b3434874c66881723e9a27c
Natenom <natenom@natenom.com>
2016-01-07 15:28:06 +0100

    new local directory for km.sh

--- mumble/km.sh
+++ mumble/km.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # "GUI" tool to start Mumble :)
+
 _kdialogtext="Choose your mumble client..."
 _kdialogstring=""
-APPDIR="/home/apps/mumble"
+APPDIR="/home/datenschleuder/apps/mumble"
 mumbles=""
 
 function readAvailableMumbles() {
     local _count=0
     for _entry in $(ls -D "${APPDIR}") #List only directories. #Does not work with spaces in dirnames.
-    do  
+    do
         mumbles[$_count]="${_entry}"
         _kdialogstring="${_kdialogstring} ${_count} \"${_entry}\" off"
 	_count=$(($_count+1))
@@ -25,6 +26,6 @@ then
     exit 1
 else
     cd "${APPDIR}/${mumbles[${_selection}]}"
-    LD_LIBRARY_PATH=./ ./mumble -m $@
+    LD_LIBRARY_PATH=./ ./mumble -m $@ &
     exit 0
-fi	
+fi
